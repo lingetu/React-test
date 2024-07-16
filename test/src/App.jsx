d07b436a513565bf97cc129879046cd1149b6ec1
LingEtu <lucas.bondon@etu.univ-amu.fr>
2024-07-16 12:14:26 +0200

    1

---
+++ test/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return(
+    <div className="container">
+      <h1>Hello, World</h1>
+      <p>I am writing JSX</p>
+    </div>
+  )
+}
+
+export default App;
